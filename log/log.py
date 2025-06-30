32b1c5b91ee89213e4611c3aabd825aba3a67641
Manish12345p <manishmaity39@gmail.com>
2025-07-01 01:53:41 +0530

    log fixed

--- log/log.py
+++ log/log.py
@@ -1,8 +1,16 @@
 from fastapi import  Request
+from http import HTTPStatus
 import logging
-logging.basicConfig(level=logging.INFO, format='Time : %(asctime)s | Level : %(levelname)s | Message : %(message)s')
+logging.basicConfig(level=logging.INFO, format='Time : %(asctime)s | Level : %(levelname)s | %(message)s ')
 async def log_requests(request: Request, call_next):
-    logging.info(f"Request: {request.method} {request.url}")
     response = await call_next(request)
-    logging.info(f"Response: {response.status_code}")
+    try:
+        if response.status_code >=200 and response.status_code < 300:
+            logging.info(f"Method: {request.method} | Url : {request.url} | Status : {response.status_code} {HTTPStatus(response.status_code).phrase}")
+        elif response.status_code >=300 and response.status_code < 400:
+            logging.warning(f"Method: {request.method} | Url : {request.url} | Status : {response.status_code} {HTTPStatus(response.status_code).phrase}")  
+        elif response.status_code >=400 and response.status_code < 500:
+            logging.error(f"Method: {request.method} | Url : {request.url} | Status : {response.status_code} {HTTPStatus(response.status_code).phrase}")
+    except Exception as e:
+        logging.error(f"Method:{request.method} | Url : {request.url} | Status:{response.status_code} {HTTPStatus(response.status_code).phrase} | Error logging request: {e}")
     return response
