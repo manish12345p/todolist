0e964440ff42682fb54a07ccb805a5dbcc80f6b2
tajindersigh111 <tajindsingh012@gmail.com>
2025-07-03 02:07:50 +0530

    add validation

--- api/v1/models.py
+++ api/v1/models.py
@@ -10,6 +10,33 @@ class TodoItemCreate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
 
+
+
+
+    @validator('title')
+    def title_must_not_be_empty(cls, v):
+        if v is not None and v.strip() == "":
+            raise ValueError("Title cannot be empty.")
+        return v
+
+    @validator('description')
+    def description_min_length(cls, v):
+        if v is not None and len(v) < 10:
+            raise ValueError("Description must be at least 10 characters long.")
+        return v
+
 class TodoItemUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
+
+    @validator('title')
+    def title_must_not_be_empty(cls, v):
+        if v is not None and v.strip() == "":
+            raise ValueError("Title cannot be empty.")
+        return v
+
+    @validator('description')
+    def description_min_length(cls, v):
+        if v is not None and len(v) < 10:
+            raise ValueError("Description must be at least 10 characters long.")
+        return v
